4b6e761b90a5b8e0f428bb18b5d30d84528e6bbb
Kirill Biakov <kbiakov@gmail.com>
2017-09-02 16:07:32 +0300

    Code cleanup

--- example/src/main/java/io/github/kbiakov/codeviewexample/ListingsActivity.java
+++ example/src/main/java/io/github/kbiakov/codeviewexample/ListingsActivity.java
@@ -29,112 +29,102 @@ public class ListingsActivity extends AppCompatActivity {
 
         final CodeView codeView = (CodeView) findViewById(R.id.code_view);
 
-        /*
-         * 1: set code content
-         */
-
-        // auto language recognition
-        codeView.setCode(getString(R.string.listing_js));
-
-        // specify language for code listing
-        codeView.setCode(getString(R.string.listing_py), "py");
-
-        /*
-         * 2: working with options
-         */
-
-        // you can change params as follows (unsafe, initialized view only)
-        codeView.getOptions()
-                .withCode(R.string.listing_java)
-                .withTheme(ColorTheme.MONOKAI);
-
-        // short initialization with default params (can be expanded using with() methods)
-        codeView.setOptions(Options.Default.get(this)
-                .withLanguage("python")
-                .withCode(R.string.listing_py)
-                .withTheme(ColorTheme.MONOKAI)
-                .withFont(Font.Consolas));
-
-        // expanded form of initialization
-        codeView.setOptions(new Options(
-                this,                                   // context
-                getString(R.string.listing_js),         // code
-                "js",                                   // language
-                ColorTheme.MONOKAI.theme(),             // theme (data)
-                FontCache.get(this).getTypeface(this),  // font
-                Format.Default.getCompact(),            // format
-                true,                                   // animate on highlight
-                true,                                   // shadows visible
-                true,                                   // shortcut
-                getString(R.string.show_all),           // shortcut note
-                10,                                     // max lines
-                new OnCodeLineClickListener() {         // line click listener
-                    @Override
-                    public void onCodeLineClicked(int n, @NotNull String line) {
-                        Log.i("ListingsActivity", "On " + (n + 1) + " line clicked");
-                    }
-                }));
-
-        /*
-         * 3: color themes
-         */
-
-        codeView.getOptions().setTheme(ColorTheme.SOLARIZED_LIGHT);
-
-        // custom theme
-        ColorThemeData myTheme = ColorTheme.SOLARIZED_LIGHT.theme()
-                .withBgContent(android.R.color.black)
-                .withNoteColor(android.R.color.white);
-
-        codeView.getOptions().setTheme(myTheme);
-
-        /*
-         * 4: custom adapter with footer views
-         */
-
-        final CustomAdapter myAdapter = new CustomAdapter(this, getString(R.string.listing_md));
-        codeView.setAdapter(myAdapter);
-        codeView.getOptions()
-                .withLanguage("md")
-                .addCodeLineClickListener(new OnCodeLineClickListener() {
-                    @Override
-                    public void onCodeLineClicked(int n, @NotNull String line) {
-                        myAdapter.addFooterEntity(n, new CustomAdapter.CustomModel("Line " + (n + 1), line));
-                    }
-                });
-
-        /*
-         * 5: diff adapter with footer views
-         */
-
-        final CodeWithDiffsAdapter diffsAdapter = new CodeWithDiffsAdapter(this);
-        codeView.getOptions()
-                .withLanguage("python")
-                .setCode(getString(R.string.listing_py));
-        codeView.updateAdapter(diffsAdapter);
-
-        diffsAdapter.addFooterEntity(16, new DiffModel(getString(R.string.py_addition_16), true));
-        diffsAdapter.addFooterEntity(11, new DiffModel(getString(R.string.py_deletion_11), false));
-
-        /*
-         * 6: shortcut adapter with footer views
-         */
-
-        codeView.getOptions()
-                .shortcut(10, "Show all");
+//        /*
+//         * 1: set code content
+//         */
+//
+//        // auto language recognition
+//        codeView.setCode(getString(R.string.listing_js));
+//
+//        // specify language for code listing
+//        codeView.setCode(getString(R.string.listing_py), "py");
+//
+//        /*
+//         * 2: working with options
+//         */
+//
+//        // you can change params as follows (unsafe, initialized view only)
+//        codeView.getOptions()
+//                .withCode(R.string.listing_java)
+//                .withTheme(ColorTheme.MONOKAI);
+//
+//        // short initialization with default params (can be expanded using with() methods)
+//        codeView.setOptions(Options.Default.get(this)
+//                .withLanguage("python")
+//                .withCode(R.string.listing_py)
+//                .withTheme(ColorTheme.MONOKAI)
+//                .withFont(Font.Consolas));
+//
+//        // expanded form of initialization
+//        codeView.setOptions(new Options(
+//                this,                                   // context
+//                getString(R.string.listing_js),         // code
+//                "js",                                   // language
+//                ColorTheme.MONOKAI.theme(),             // theme (data)
+//                FontCache.get(this).getTypeface(this),  // font
+//                Format.Default.getCompact(),            // format
+//                true,                                   // animate on highlight
+//                true,                                   // shadows visible
+//                true,                                   // shortcut
+//                getString(R.string.show_all),           // shortcut note
+//                10,                                     // max lines
+//                new OnCodeLineClickListener() {         // line click listener
+//                    @Override
+//                    public void onCodeLineClicked(int n, @NotNull String line) {
+//                        Log.i("ListingsActivity", "On " + (n + 1) + " line clicked");
+//                    }
+//                }));
+//
+//        /*
+//         * 3: color themes
+//         */
+//
+//        codeView.getOptions().setTheme(ColorTheme.SOLARIZED_LIGHT);
+//
+//        // custom theme
+//        ColorThemeData myTheme = ColorTheme.SOLARIZED_LIGHT.theme()
+//                .withBgContent(android.R.color.black)
+//                .withNoteColor(android.R.color.white);
+//
+//        codeView.getOptions().setTheme(myTheme);
+//
+//        /*
+//         * 4: custom adapter with footer views
+//         */
+//
+//        final CustomAdapter myAdapter = new CustomAdapter(this, getString(R.string.listing_md));
+//        codeView.setAdapter(myAdapter);
+//        codeView.getOptions()
+//                .withLanguage("md")
+//                .addCodeLineClickListener(new OnCodeLineClickListener() {
+//                    @Override
+//                    public void onCodeLineClicked(int n, @NotNull String line) {
+//                        myAdapter.addFooterEntity(n, new CustomAdapter.CustomModel("Line " + (n + 1), line));
+//                    }
+//                });
+//
+//        /*
+//         * 5: diff adapter with footer views
+//         */
+//
+//        final CodeWithDiffsAdapter diffsAdapter = new CodeWithDiffsAdapter(this);
+//        codeView.getOptions()
+//                .withLanguage("python")
+//                .setCode(getString(R.string.listing_py));
+//        codeView.updateAdapter(diffsAdapter);
+//
+//        diffsAdapter.addFooterEntity(16, new DiffModel(getString(R.string.py_addition_16), true));
+//        diffsAdapter.addFooterEntity(11, new DiffModel(getString(R.string.py_deletion_11), false));
+//
+//        /*
+//         * 6: shortcut adapter with footer views
+//         */
+//
+//        codeView.getOptions()
+//                .shortcut(10, "Show all");
 
         // - Playground
-        
-        codeView.updateOptions(new Function1<Options, Unit>() {
-            @Override
-            public Unit invoke(Options options) {
-                options.withFont(Font.Consolas)
-                        .withTheme(ColorTheme.SOLARIZED_LIGHT)
-                        .withShadows()
-                        .setShortcut(false);
-                return null;
-            }
-        });
+
         codeView.setCode("" +
                 "package io.github.kbiakov.codeviewexample;\n" +
                 "\n" +
@@ -175,5 +165,15 @@ public class ListingsActivity extends AppCompatActivity {
                 "        codeView.setCode(getString(R.string.listing_py), \"py\");" +
                 "    }\n" +
                 "}", "java");
+        codeView.updateOptions(new Function1<Options, Unit>() {
+            @Override
+            public Unit invoke(Options options) {
+                options.withFont(Font.Consolas)
+                        .withTheme(ColorTheme.SOLARIZED_LIGHT)
+                        .withShadows()
+                        .setShortcut(false);
+                return null;
+            }
+        });
     }
 }
