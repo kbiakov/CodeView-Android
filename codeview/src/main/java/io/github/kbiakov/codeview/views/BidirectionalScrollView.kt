5b7b8184d01f089eb2f474fe2da0ba47cc355c33
LinZong <353388639@qq.com>
2021-05-05 11:56:59 +0800

    Fix RecyclerView layouts all items regardless remaining spaces

--- codeview/src/main/java/io/github/kbiakov/codeview/views/BidirectionalScrollView.kt
+++ codeview/src/main/java/io/github/kbiakov/codeview/views/BidirectionalScrollView.kt
@@ -1,12 +1,15 @@
 package io.github.kbiakov.codeview.views
 
 import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.view.View.MeasureSpec
 import android.view.View.MeasureSpec.makeMeasureSpec
 import android.widget.HorizontalScrollView
-import io.github.kbiakov.codeview.dpToPx
+import io.github.kbiakov.codeview.R
 
 /**
  * @class BidirectionalScrollView
@@ -18,67 +21,61 @@ import io.github.kbiakov.codeview.dpToPx
  */
 class BidirectionalScrollView : HorizontalScrollView {
 
-    private var currentX = 0
-    private var currentY = 0
-    private var isMoved = false
-
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                currentX = event.rawX.toInt()
-                currentY = event.rawY.toInt()
-                return super.dispatchTouchEvent(event)
-            }
-            MotionEvent.ACTION_MOVE -> {
-                val deltaX = Math.abs(currentX - event.rawX)
-                val deltaY = Math.abs(currentY - event.rawY)
-                scroll(event)
+    private lateinit var codeContentRv: RecyclerView
 
-                val movedOnDistance = dpToPx(context, 2)
-                if (deltaX > movedOnDistance || deltaY > movedOnDistance) {
-                    isMoved = true
-                }
-            }
-            MotionEvent.ACTION_UP -> {
-                if (!isMoved) {
-                    return super.dispatchTouchEvent(event)
-                }
-                isMoved = false
-            }
-            MotionEvent.ACTION_CANCEL -> {
-                isMoved = false
-            }
-        }
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        codeContentRv = findViewById(R.id.rv_code_content)
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        super.dispatchTouchEvent(event)
+        // consumed by this ViewGroup.
         return true
     }
 
-    private fun scroll(event: MotionEvent) {
-        val x2 = event.rawX.toInt()
-        val y2 = event.rawY.toInt()
-        val posX = currentX - x2
-        val posY = currentY - y2
-        scrollBy(posX, posY)
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        super.onInterceptTouchEvent(ev)
+        // let touch event be stolen(intercepted) here.
+        return true
+    }
 
-        currentX = x2
-        currentY = y2
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        // first, force child recyclerview handles vertical scrolling.
+        codeContentRv.onTouchEvent(ev)
+        // then handle horizontal scrolling here.
+        super.onTouchEvent(ev)
+        // finally mark this event is handled.
+        return true
     }
 
     override fun measureChild(child: View, parentWidthMeasureSpec: Int, parentHeightMeasureSpec: Int) {
         val zeroMeasureSpec = makeMeasureSpec(0)
-        child.measure(zeroMeasureSpec, zeroMeasureSpec)
+        // let the child RecyclerView know the actual height.
+        child.measure(zeroMeasureSpec, parentHeightMeasureSpec)
     }
 
     override fun measureChildWithMargins(
-            child: View,
-            parentWidthMeasureSpec: Int, widthUsed: Int,
-            parentHeightMeasureSpec: Int, heightUsed: Int
+        child: View,
+        parentWidthMeasureSpec: Int, widthUsed: Int,
+        parentHeightMeasureSpec: Int, heightUsed: Int
     ) = with(child.layoutParams as MarginLayoutParams) {
+
         val widthMeasureSpec = makeMeasureSpec(leftMargin + rightMargin, MeasureSpec.UNSPECIFIED)
-        val heightMeasureSpec = makeMeasureSpec(topMargin + bottomMargin, MeasureSpec.UNSPECIFIED)
+        /**
+         * Let the child RecyclerView know the actual height again.
+         * Because [RecyclerView.LayoutManager.mHeightMode] is determined by MeasureSpec mode in [RecyclerView.onMeasure].
+         * If gives a [MeasureSpec.UNSPECIFIED] here, mHeightMode will be 0 and [LinearLayoutManager.LayoutState.mInfinite] will be true.
+         * It will cause [LinearLayoutManager.fill] iterates all items in adapter instead of remaining space allowed,
+         * which blocks main thread for a long time if there are many items in adapter,
+         */
+        val heightMode = MeasureSpec.getMode(parentHeightMeasureSpec)
+        val parentHeight = MeasureSpec.getSize(parentHeightMeasureSpec)
+        val heightMeasureSpec = makeMeasureSpec(parentHeight + topMargin + bottomMargin, heightMode)
         child.measure(widthMeasureSpec, heightMeasureSpec)
     }
 
